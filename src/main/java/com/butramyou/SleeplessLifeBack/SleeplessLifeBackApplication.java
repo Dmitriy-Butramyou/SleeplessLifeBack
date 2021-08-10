fb2db7a8e2dcd14bc451706677545ab31ebcf186
D_Butramyou <d.butramyou@gmail.com>
2021-08-10 16:03:53 +0300

    Initial commit

---
+++ src/main/java/com/butramyou/SleeplessLifeBack/SleeplessLifeBackApplication.java
@@ -0,0 +1,13 @@
+package com.butramyou.SleeplessLifeBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleeplessLifeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleeplessLifeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/butramyou/SleeplessLifeBack/SleeplessLifeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butramyou.SleeplessLifeBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleeplessLifeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
